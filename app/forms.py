22ed4f172e0848108d4090476e73870c654fec36
Alan Zavala <alan.j.zavala@gmail.com>
2025-05-18 20:42:25 -0500

    added other page forms and working calculatoins

--- app/forms.py
+++ app/forms.py
@@ -6,7 +6,24 @@ from app.models import Element
 class InWaterForm(FlaskForm):
     m_water = FloatField('Mass of water', validators=[InputRequired()])
     t_water = FloatField('Temperature of water', validators=[InputRequired()])
-    c_subject = FloatField('Specific Heat of object', validators=[InputRequired()])
-    m_subject = FloatField('Mass of object', validators=[InputRequired()])
-    t_subject = FloatField('Temperature of object', validators=[InputRequired()])
+    c_object = FloatField('Specific Heat of object', validators=[InputRequired()])
+    m_object = FloatField('Mass of object', validators=[InputRequired()])
+    t_object = FloatField('Temperature of object', validators=[InputRequired()])
+    submit = SubmitField("Calculate Final Temperature")
+
+class InContactForm(FlaskForm):
+    m_1 = FloatField('Mass 1', validators=[InputRequired()])
+    t_1 = FloatField('Temperature 1', validators=[InputRequired()])
+    c_1 = FloatField('Specific Heat 1', validators=[InputRequired()])
+    m_2 = FloatField('Mass 2', validators=[InputRequired()])
+    t_2 = FloatField('Temperature 2', validators=[InputRequired()])
+    c_2 = FloatField('Specific Heat 2', validators=[InputRequired()])
+    submit = SubmitField("Calculate Final Temperature")
+
+class InMarsForm(FlaskForm):
+    m_water = FloatField('Mass of water', validators=[InputRequired()])
+    t_water = FloatField('Temperature of water', validators=[InputRequired()])
+    c_object = FloatField('Specific Heat of object', validators=[InputRequired()])
+    m_object = FloatField('Mass of object', validators=[InputRequired()])
+    t_object = FloatField('Temperature of object', validators=[InputRequired()])
     submit = SubmitField("Calculate Final Temperature")
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, session, request, redirect, url_for, flash, c
 from app import db
 from app.models import Element
 #from .calculations import in_water, in_contact
-from app.forms import InWaterForm
+from app.forms import InWaterForm, InContactForm, InMarsForm
 
 @app.route('/')
 def main():
@@ -12,28 +12,89 @@ def main():
 def in_water():
     form = InWaterForm()
     if form.validate_on_submit():
-        # Create a new Product entry
-        
-        return redirect(url_for('calculate_result'))
-     
-    else:
+        try:
+            m_water = float(form.m_water.data)
+            t_water = float(form.t_water.data)
+            c_water = 4.184  # J/g·°C
+            t_object = float(form.t_object.data)
+            m_object = float(form.m_object.data)
+            c_object = float(form.c_object.data)
+
+            cm_water = c_water * m_water
+            cm_object = c_object * m_object
+            result = (cm_object * t_object + cm_water * t_water) / (cm_object + cm_water)
+            result = round(result, 2)
+
+            return render_template('in_water.html', form=form, result=result, m1=form.m_water.data,T1=form.t_water.data,
+                                   T2=form.t_object.data,m2=form.m_object.data,c2=form.c_object.data)
+
+        except (TypeError, ValueError):
+            flash("Invalid input. Please enter valid numbers.")
+            return render_template('in_water.html', form=form)
+
+    if request.method == 'POST':
         flash('All fields are required.')
 
     return render_template('in_water.html', form=form)
 
+
 @app.route('/in_waterLatent', methods=['POST', 'GET'])
 def in_waterLatent():
-    #form = InWaterForm()
-    #water_temp = float(form.water_T.data)
+    form = InWaterForm()
+    if form.validate_on_submit():
+        try:
+            m_water = float(form.m_water.data)  # g 
+            t_water = float(form.t_water.data)  # C
+            c_water = 4.184  # J/g·°C
+            t_object = float(form.t_object.data)
+            m_object = float(form.m_object.data)
+            c_object = float(form.c_object.data)
 
-    return render_template('in_waterLatent.html')
+            cm_water = c_water * m_water
+            cm_object = c_object * m_object
+            result = (cm_object * t_object + cm_water * t_water) / (cm_object + cm_water)
+            result = round(result, 2)
+
+            return render_template('in_waterLatent.html', form=form, result=result, m1=form.m_water.data,T1=form.t_water.data,
+                                   T2=form.t_object.data,m2=form.m_object.data,c2=form.c_object.data)
+
+        except (TypeError, ValueError):
+            flash("Invalid input. Please enter valid numbers.")
+            return render_template('in_waterLatent.html', form=form)
+
+    if request.method == 'POST':
+        flash('All fields are required.')
+
+    return render_template('in_waterLatent.html', form = form)
 
 @app.route('/in_contact', methods=['POST', 'GET'])
 def in_contact():
-    #form = InWaterForm()
-    #water_temp = float(form.water_T.data)
+    form = InContactForm()
+    if form.validate_on_submit():
+        try:
+            m_1 = float(form.m_1.data)  # g
+            t_1 = float(form.t_1.data)  # C
+            c_1 = float(form.c_1.data)  # J/g·°C
+            t_2 = float(form.t_2.data)  
+            m_2 = float(form.m_2.data)
+            c_2 = float(form.c_2.data)
 
-    return render_template('in_contact.html')
+            cm_1 = c_1 * m_1
+            cm_2 = c_2 * m_2
+            result = (cm_1 * t_1 + cm_2 * t_2) / (cm_1 + cm_2)
+            result = round(result, 2)
+
+            return render_template('in_contact.html', form=form, result=result, m1=form.m_1.data,T1=form.t_1.data,
+                                   c1=form.c_1.data, T2=form.t_2.data,m2=form.m_2.data,c2=form.c_2.data)
+
+        except (TypeError, ValueError):
+            flash("Invalid input. Please enter valid numbers.")
+            return render_template('in_contact.html', form=form)
+
+    if request.method == 'POST':
+        flash('All fields are required.')
+
+    return render_template('in_contact.html', form=form)
 
 @app.route('/mars', methods=['POST', 'GET'])
 def mars():
